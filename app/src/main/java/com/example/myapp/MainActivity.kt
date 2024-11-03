180baa51c2c139b1851b818e91503ec60e00dd56
Hernan Febri <hernanfebri@gmail.com>
2024-11-03 11:48:10 +0700

    Cara penggunaan implisit intent

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,44 +1,81 @@
 package com.example.myapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.google.android.material.snackbar.Snackbar
 
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity() : AppCompatActivity(), Parcelable {
 
 
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val editText: EditText = findViewById(R.id.edit_text)
         val btnClick: Button =  findViewById(R.id.btn_click)
-        val mainLayout: ConstraintLayout = findViewById(R.id.main_layout)
+        val btnSecond: Button = findViewById(R.id.btn_second)
 
 
-        btnClick.setOnClickListener{
-            val teks: String = editText.text.toString()
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("My App")
-            builder.setMessage(teks)
-            builder.setPositiveButton("Tutup"){
-                dialog, which ->
-                dialog.dismiss()
+        btnSecond.setOnClickListener {
+            // IMPLISIT INTENT untuk membuka URL di Google Chrome
+//            val url = "https://chat.openai.com"
+//            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+//                setPackage("com.android.chrome") // Memastikan membuka di Chrome
+//            }
+//
+//            // Memeriksa apakah ada aplikasi yang dapat menangani intent ini
+//            if (intent.resolveActivity(packageManager) != null) {
+//                startActivity(intent)
+//            } else {
+//                // Jika Chrome tidak terinstal, buka dengan cara lain
+//                val fallbackIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+//                startActivity(fallbackIntent)
+//            }
+//        }
+
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+
+                type = "text/plain"
             }
-
-            val dialog = builder.create()
-            dialog.show()
+            intent.putExtra(Intent.EXTRA_TEXT, "Hai, ini adalah kiriman")
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Tidak ada aplikasi yang support",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/myapp/SecondActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapp
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textView: TextView =  findViewById(R.id.text)
+
+        if (intent.hasExtra("text")){
+            textView.text = intent.getStringExtra("text")
+        }else{
+            Toast.makeText(applicationContext, "Tidak ada data", Toast.LENGTH_SHORT)
+        }
+    }
+}
